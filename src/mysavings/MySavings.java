c5e76f52a4e57cb9040efe5379f83424130fc3f7
rodec8600 <rodec8600@7J0N6Y1.Student.UGDSB.ED>
2016-10-17 13:46:18 -0400

    git pls

---
+++ src/mysavings/MySavings.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mysavings;
+
+/**
+ *
+ * @author rodec8600
+ */
+public class MySavings {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
